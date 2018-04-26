1a16db9ac4a231c9f0edaa869fadc73a633f39ee
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-04-26 13:58:40 +0800

    1：修改prod环境配置。

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,8 +2,8 @@ import { DefaultConfig } from './config.default';
 
 export default () => {
   const config: DefaultConfig = {};
-  config.loginInUrl = 'http://127.0.0.1:7001/';
-  config.homepage = 'http://localhost:8080/#/';
+  config.loginInUrl = 'http://123.206.95.15:7001/';
+  config.homepage = 'http://123.206.95.15:8080/#/';
   config.appId = 'HAGzPVNMVnt0WA4KJs9qmHme-gzGzoHsz';
   config.appKey = 'NTXdKfBeztXeXb4rpVEs38yQ';
   config.dump = {
