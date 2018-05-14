12554ab8a47935ceed0395393b48a5dde1d08b0f
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-05-14 20:28:44 +0800

    1：登录页面。

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,7 +2,7 @@ import { DefaultConfig } from './config.default';
 
 export default () => {
   const config: DefaultConfig = {};
-  config.loginInUrl = 'http://193.112.31.164:7001/';
+  config.loginInUrl = 'http://193.112.31.164:8080/#/login';
   config.homepage = 'http://193.112.31.164:8080/#/';
   config.appId = 'HAGzPVNMVnt0WA4KJs9qmHme-gzGzoHsz';
   config.appKey = 'NTXdKfBeztXeXb4rpVEs38yQ';
