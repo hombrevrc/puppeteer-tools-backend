5eb66e9ed134f533e4c1cc528821d88124f4d87b
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-05-14 20:35:00 +0800

    1：修改配置。

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,8 +2,8 @@ import { DefaultConfig } from './config.default';
 
 export default () => {
   const config: DefaultConfig = {};
-  config.loginInUrl = 'http://193.112.31.164:8080/#/login';
-  config.homepage = 'http://193.112.31.164:8080/#/';
+  config.loginInUrl = 'http://193.112.31.164:8080/puppeteer-tools/#/login';
+  config.homepage = 'http://193.112.31.164:8080/puppeteer-tools/#/';
   config.appId = 'HAGzPVNMVnt0WA4KJs9qmHme-gzGzoHsz';
   config.appKey = 'NTXdKfBeztXeXb4rpVEs38yQ';
   config.dump = {
