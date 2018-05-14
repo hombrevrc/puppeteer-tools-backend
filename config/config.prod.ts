4226c5b0cac296bdc76ce0f64c4c57245e28a225
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-05-14 11:03:14 +0800

    1：修改配置信息。

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,8 +2,8 @@ import { DefaultConfig } from './config.default';
 
 export default () => {
   const config: DefaultConfig = {};
-  config.loginInUrl = 'http://123.206.95.15:7001/';
-  config.homepage = 'http://123.206.95.15:8080/#/';
+  config.loginInUrl = 'http://193.112.31.164:7001/';
+  config.homepage = 'http://193.112.31.164:8080/#/';
   config.appId = 'HAGzPVNMVnt0WA4KJs9qmHme-gzGzoHsz';
   config.appKey = 'NTXdKfBeztXeXb4rpVEs38yQ';
   config.dump = {
