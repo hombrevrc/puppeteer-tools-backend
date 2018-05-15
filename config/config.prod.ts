1f47433602398f0bb1cad033f4f2b00605b7b78b
yanguoyu_cd <yanguoyu_cd@keruyun.com>
2018-05-15 09:39:34 +0800

    1：线上环境配置github的key。

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -9,6 +9,10 @@ export default () => {
   config.dump = {
     ignore: new Set(['appId', 'appKey'])
   };
+  config.passportGithub = {
+    key: '28a839110d85959f990e',
+    secret: '8936c89432e4e1e054993926db3f785968567675'
+  };
 
   config.security = {
     csrf: {
